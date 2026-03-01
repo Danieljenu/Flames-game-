e6f7a0c6b46f8ea368b56ee73a9ce8e2fb29ff55
Daniel jenu <danieljenuthayyil@gmail.com>
2026-03-01 21:28:39 +0000

    fixed code

--- main.py
+++ main.py
@@ -1,33 +1,31 @@
-    b=input("Enter your name ")
-    g=input("Enter your partner name")
-    el1=[]
-    el2=[]
-    for e1 in b:
-        el1.append(e1)
-    for e2 in g:
-        el2.append(e2)
-    print(el1,el2)
-    #for removing comman thing in the listi
-    for f in el1[:]:          # iterate over a COPY
-        if f in el2:
-            el1.remove(f)
-            el2.remove(f)
-    oflame=len(el1)+len(el2)
-    n=0
-    def jok():
-        global flame
-        while True:
-            if len(l)<flame:
-                flame=-len(l)
-            else:
-                l.pop(flame-1)
-                n=flame
-                flame=oflame
-    l=['F','L','A','M','E','S']
-    flame=oflame
-    while len(l)>=2:
-        if n==0:
-            jok()
-        else:
-            flame=-n
-            jok()
+b = input("Enter your name: ").upper().replace(" ", "")
+g = input("Enter your partner name: ").upper().replace(" ", "")
+
+el1 = list(b)
+el2 = list(g)
+
+# Remove common letters
+for ch in el1[:]:
+    if ch in el2:
+        el1.remove(ch)
+        el2.remove(ch)
+
+count = len(el1) + len(el2)
+
+l = ['F', 'L', 'A', 'M', 'E', 'S']
+index = 0
+
+while len(l) > 1:
+    index = (index + count - 1) % len(l)
+    l.pop(index)
+
+result = {
+    'F': 'Friends',
+    'L': 'Love',
+    'A': 'Affection',
+    'M': 'Marriage',
+    'E': 'Enemy',
+    'S': 'Siblings'
+}
+
+print("Relationship:", result[l[0]])
